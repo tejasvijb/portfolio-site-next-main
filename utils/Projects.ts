7b89eadcbd0595ddb4acc99aeab18826a6eced76
tejasvijb <jbtejasvi@gmail.com>
2022-12-06 03:18:09 +0530

    fix video links

--- utils/Projects.ts
+++ utils/Projects.ts
@@ -3,14 +3,14 @@ export const projects = [
     name: 'QKart Frontend',
     descreption:
       'QKart is an e-commerce application offering a variety of products for customers to choose from.',
-    videoSrc: 'https://res.cloudinary.com/divpcushv/video/upload/v1670245041/QKartcompressedmp4_iifded.mp4',
+    videoSrc: 'https://res.cloudinary.com/divpcushv/video/upload/v1670276787/Qkartcompressedmp42_cyfmvu.mp4',
     tags: ['React', 'JavaScript', 'PostgreSQL', 'Express', 'Material UI', 'Debouncing'],
     links: ['https://github.com/tejasvijb/qkartfrontend', 'https://cutt.ly/51CGON7'],
   },
   {
     name: 'XBoard',
     descreption: 'XBoard is a News Feed website that will feature the latest news for select topics from Flipboard.',
-    videoSrc: 'https://res.cloudinary.com/divpcushv/video/upload/v1670245407/CriofeedCompressed_zjyuq7.mp4',
+    videoSrc: 'https://res.cloudinary.com/divpcushv/video/upload/v1670276787/Criofeedcompressed2_kueja6.mp4',
     tags: ['JavaScript', 'Bootstrap', 'HTML', 'CSS', 'REST', 'ES6', 'JSON'],
     links: ['https://cutt.ly/41CG1Zy', 'https://cutt.ly/41CG1Zy'],
   },
